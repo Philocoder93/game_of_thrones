57f0236f37806327451b5db86b241480098249ae
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-02-24 16:53:37 -0500

    seeds updated to include more content (no change in functionality)

--- game_of_thrones/db/seeds.rb
+++ game_of_thrones/db/seeds.rb
@@ -2,4 +2,15 @@ House.destroy_all
 Character.destroy_all
 
 targaryen = House.create({name:'targaryen',img_url:'http://awoiaf.westeros.org/images/thumb/1/1e/House_Targaryen.svg/250px-House_Targaryen.svg.png',house_word:'fire_and_blood'})
+stark = House.create({name:'stark',img_url:'http://awoiaf.westeros.org/images/thumb/1/1e/House_Targaryen.svg/250px-House_Targaryen.svg.png',house_word:'its a bt nippy out here'})
+baratheon = House.create({name:'baratheon',img_url:'http://awoiaf.westeros.org/images/thumb/1/1e/House_Targaryen.svg/250px-House_Targaryen.svg.png',house_word:'some stuff'})
+tyrell = House.create({name:'tyrell',img_url:'http://awoiaf.westeros.org/images/thumb/1/1e/House_Targaryen.svg/250px-House_Targaryen.svg.png',house_word:'whatever'})
+
+targaryen.characters.create({name:'arys',dead:true,img_url:'http://www.telegraph.co.uk/content/dam/tv/2016/06/01/mad-king-aerys-targaryen-large_trans_NvBQzQNjv4Bq3480UNUU8UfSxDSaY1n7MBMSxGIR1rd_-iNIxL4YeIk.jpg'})
+targaryen.characters.create({name:'arys',dead:true,img_url:'http://www.telegraph.co.uk/content/dam/tv/2016/06/01/mad-king-aerys-targaryen-large_trans_NvBQzQNjv4Bq3480UNUU8UfSxDSaY1n7MBMSxGIR1rd_-iNIxL4YeIk.jpg'})
+targaryen.characters.create({name:'arys',dead:true,img_url:'http://www.telegraph.co.uk/content/dam/tv/2016/06/01/mad-king-aerys-targaryen-large_trans_NvBQzQNjv4Bq3480UNUU8UfSxDSaY1n7MBMSxGIR1rd_-iNIxL4YeIk.jpg'})
+targaryen.characters.create({name:'arys',dead:true,img_url:'http://www.telegraph.co.uk/content/dam/tv/2016/06/01/mad-king-aerys-targaryen-large_trans_NvBQzQNjv4Bq3480UNUU8UfSxDSaY1n7MBMSxGIR1rd_-iNIxL4YeIk.jpg'})
+targaryen.characters.create({name:'arys',dead:true,img_url:'http://www.telegraph.co.uk/content/dam/tv/2016/06/01/mad-king-aerys-targaryen-large_trans_NvBQzQNjv4Bq3480UNUU8UfSxDSaY1n7MBMSxGIR1rd_-iNIxL4YeIk.jpg'})
+targaryen.characters.create({name:'arys',dead:true,img_url:'http://www.telegraph.co.uk/content/dam/tv/2016/06/01/mad-king-aerys-targaryen-large_trans_NvBQzQNjv4Bq3480UNUU8UfSxDSaY1n7MBMSxGIR1rd_-iNIxL4YeIk.jpg'})
+targaryen.characters.create({name:'arys',dead:true,img_url:'http://www.telegraph.co.uk/content/dam/tv/2016/06/01/mad-king-aerys-targaryen-large_trans_NvBQzQNjv4Bq3480UNUU8UfSxDSaY1n7MBMSxGIR1rd_-iNIxL4YeIk.jpg'})
 targaryen.characters.create({name:'arys',dead:true,img_url:'http://www.telegraph.co.uk/content/dam/tv/2016/06/01/mad-king-aerys-targaryen-large_trans_NvBQzQNjv4Bq3480UNUU8UfSxDSaY1n7MBMSxGIR1rd_-iNIxL4YeIk.jpg'})
